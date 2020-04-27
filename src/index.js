5a70d247e8cad7624b9e138ea877a7ad41d62b03
namlb <namlb1606@gmail.com>
2020-04-27 17:17:19 +0700

    routing

--- src/index.js
+++ src/index.js
@@ -2,16 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { Route, BrowserRouter as Router } from 'react-router-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const routing = (
+  <Router>
+    <div>
+      <Route path="/device_status" component={App} />
+    </div>
+  </Router>
+)
+ReactDOM.render(routing, document.getElementById('root'))
